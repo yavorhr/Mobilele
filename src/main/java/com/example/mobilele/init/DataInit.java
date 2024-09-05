d97b90af75effc5a050140cb5d94d4f1d8499dd9
wolfie <yavorhristozov@gmail.com>
2024-09-05 19:11:24 +0300

    Add : DataInit class
    - Start point of the application

---
+++ src/main/java/com/example/mobilele/init/DataInit.java
@@ -0,0 +1,13 @@
+package com.example.mobilele.init;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DataInit implements CommandLineRunner {
+
+  @Override
+  public void run(String... args) throws Exception {
+
+  }
+}
