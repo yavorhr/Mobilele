4cb2115fba19a8dc2adbed79c9f2226fd6bfcee7
wolfie <yavorhristozov@gmail.com>
2024-09-06 20:18:51 +0300

    Refact : Brand entity
    - Remove fields created and modified, incl. getters and setters
    - Remove @PostCunstroct annotation. The right one to use is @PrePersist, which is added in the BaseEntity
    - The class will inherit them from BaseEntity

--- src/main/java/com/example/mobilele/model/entity/Brand.java
+++ src/main/java/com/example/mobilele/model/entity/Brand.java
@@ -1,17 +1,14 @@
 package com.example.mobilele.model.entity;
 
-import jakarta.annotation.PostConstruct;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
-import java.time.Instant;
+
 
 @Entity
 @Table(name = "brands")
 public class Brand extends BaseEntity {
   private String name;
-  private Instant created;
-  private Instant modified;
 
   public Brand() {
   }
@@ -21,30 +18,7 @@ public class Brand extends BaseEntity {
     return name;
   }
 
-  @Column(columnDefinition = "DATETIME")
-  public Instant getCreated() {
-    return created;
-  }
-
-  @Column(columnDefinition = "DATETIME")
-  public Instant getModified() {
-    return modified;
-  }
-
   public void setName(String name) {
     this.name = name;
-  }
-
-  public void setCreated(Instant created) {
-    this.created = created;
-  }
-
-  public void setModified(Instant modified) {
-    this.modified = modified;
-  }
-
-  @PostConstruct
-  private void setCreatedTime() {
-    setCreated(Instant.now());
   }
 }
