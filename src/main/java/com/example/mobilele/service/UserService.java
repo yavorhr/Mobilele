7e3e30abaa59be7fd769df4b2fedb9d5e271c1b3
wolfie <yavorhristozov@gmail.com>
2024-09-16 22:21:58 +0300

    Update : UserSericeImpl
    - Add Implementation of boolean login(UserServiceModel) method
    - The method is returning true or false depending on that if user is existing in the database, and the input password matches with the password in the dabase
    - For comparing password is used the passwordEncoder.matches() method, because the passwords in the database are hashed.

--- src/main/java/com/example/mobilele/service/UserService.java
+++ src/main/java/com/example/mobilele/service/UserService.java
@@ -1,4 +1,7 @@
 package com.example.mobilele.service;
+import com.example.mobilele.model.dto.service.UserLoginServiceModel;
 
 public interface UserService {
+
+  boolean login (UserLoginServiceModel userLoginServiceModel);
 }
--- src/main/java/com/example/mobilele/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/mobilele/service/impl/UserServiceImpl.java
@@ -1,8 +1,33 @@
 package com.example.mobilele.service.impl;
 
+import com.example.mobilele.model.dto.service.UserLoginServiceModel;
+import com.example.mobilele.model.entity.User;
+import com.example.mobilele.repository.UserRepository;
 import com.example.mobilele.service.UserService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class UserServiceImpl implements UserService {
+  private final PasswordEncoder passwordEncoder;
+  private final UserRepository userRepository;
+
+  public UserServiceImpl(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    this.passwordEncoder = passwordEncoder;
+    this.userRepository = userRepository;
+  }
+
+  @Override
+  public boolean login(UserLoginServiceModel userLoginServiceModel) {
+
+    Optional<User> user = this.userRepository.findByUsername(userLoginServiceModel.getUsername());
+
+    if (user.isEmpty()) {
+      return false;
+    } else {
+      return passwordEncoder.matches(userLoginServiceModel.getPassword(), user.get().getPassword());
+    }
+  }
 }
