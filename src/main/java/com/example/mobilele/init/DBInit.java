89ef7390027304e9de93a1dad7af5ea1b50c1527
wolfie <yavorhristozov@gmail.com>
2024-09-23 11:24:03 +0300

    Update DBInit.java
    - Remove the initialization method of ModelCar

--- src/main/java/com/example/mobilele/init/DBInit.java
+++ src/main/java/com/example/mobilele/init/DBInit.java
@@ -6,25 +6,26 @@ import com.example.mobilele.model.entity.User;
 import com.example.mobilele.model.entity.enums.CategoryTypeEnum;
 import com.example.mobilele.repository.ModelRepository;
 import com.example.mobilele.repository.UserRepository;
+import com.example.mobilele.service.UserService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class DBInit implements CommandLineRunner {
-  private final ModelRepository modelRepository;
   private final UserRepository userRepository;
   private final PasswordEncoder passwordEncoder;
+  private final UserService userService;
 
-  public DBInit(ModelRepository modelRepository, UserRepository userRepository, PasswordEncoder passwordEncoder) {
+  public DBInit(ModelRepository modelRepository, UserRepository userRepository, PasswordEncoder passwordEncoder, UserService userService) {
     this.modelRepository = modelRepository;
     this.userRepository = userRepository;
     this.passwordEncoder = passwordEncoder;
+    this.userService = userService;
   }
 
   @Override
   public void run(String... args) throws Exception {
-    initCarModel();
     initUser();
   }
 
@@ -32,22 +33,6 @@ public class DBInit implements CommandLineRunner {
     if (this.userRepository.count() == 0) {
       User user = createUser();
       this.userRepository.save(user);
-    }
-  }
-
-  private void initCarModel() {
-    if (this.modelRepository.count() == 0) {
-      Brand brand = new Brand();
-      brand.setName("BMW");
-
-      Model x1 = new Model();
-      x1.setBrand(brand);
-      x1.setCategory(CategoryTypeEnum.CAR);
-      x1.setName("x5");
-      x1.setStartYear(2010);
-      x1.setImageUrl("https://www.bmw.bg/bg/all-models/m-series/bmw-x1-m35i/2023/bmw-x1-m35i-overview.html");
-
-      this.modelRepository.save(x1);
     }
   }
 
