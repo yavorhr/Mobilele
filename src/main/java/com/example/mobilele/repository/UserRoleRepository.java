35740dcd7661648388916762d76c79461d36ed16
wolfie <yavorhristozov@gmail.com>
2024-09-23 12:41:34 +0300

    Update UserRoleRepository.java
    Add findByRole(UserRoleEnum userRoleEnum), which returns UserRole entity

--- src/main/java/com/example/mobilele/repository/UserRoleRepository.java
+++ src/main/java/com/example/mobilele/repository/UserRoleRepository.java
@@ -1,9 +1,12 @@
 package com.example.mobilele.repository;
 
 import com.example.mobilele.model.entity.UserRole;
+import com.example.mobilele.model.entity.enums.UserRoleEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRoleRepository extends JpaRepository<UserRole, Long> {
+
+  UserRole findByRole(UserRoleEnum userRoleEnum);
 }
