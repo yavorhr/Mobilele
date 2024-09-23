d1a18809b55f5fe4774f67f358274bdec6847650
wolfie <yavorhristozov@gmail.com>
2024-09-23 12:40:24 +0300

    Update UserServiceImpl.java
    - Add initUsers() method - if the userRepository is empty, it will create two Users - one admin and one regular users, and will store them in the db

--- src/main/java/com/example/mobilele/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/mobilele/service/impl/UserServiceImpl.java
@@ -2,12 +2,16 @@ package com.example.mobilele.service.impl;
 
 import com.example.mobilele.model.dto.service.UserLoginServiceModel;
 import com.example.mobilele.model.entity.User;
+import com.example.mobilele.model.entity.UserRole;
+import com.example.mobilele.model.entity.enums.UserRoleEnum;
 import com.example.mobilele.repository.UserRepository;
+import com.example.mobilele.service.UserRoleService;
 import com.example.mobilele.service.UserService;
 import com.example.mobilele.user.CurrentUser;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,11 +19,13 @@ public class UserServiceImpl implements UserService {
   private final PasswordEncoder passwordEncoder;
   private final UserRepository userRepository;
   private final CurrentUser currentUser;
+  private final UserRoleService roleService;
 
-  public UserServiceImpl(PasswordEncoder passwordEncoder, UserRepository userRepository, CurrentUser currentUser) {
+  public UserServiceImpl(PasswordEncoder passwordEncoder, UserRepository userRepository, CurrentUser currentUser, UserRoleService roleService) {
     this.passwordEncoder = passwordEncoder;
     this.userRepository = userRepository;
     this.currentUser = currentUser;
+    this.roleService = roleService;
   }
 
   @Override
@@ -43,6 +49,8 @@ public class UserServiceImpl implements UserService {
         currentUser.setLoggedIn(true);
         currentUser.setFirstName(loggedInUser.getFirstName());
         currentUser.setLastName(loggedInUser.getLastName());
+
+        loggedInUser.getRoles().forEach(r-> currentUser.addRole(r.getRole()));
       }
 
       return success;
@@ -52,5 +60,34 @@ public class UserServiceImpl implements UserService {
   @Override
   public void logout() {
     this.currentUser.clean();
+  }
+
+  @Override
+  public void initUsers() {
+    if (userRepository.count() == 0) {
+      UserRole adminRole = this.roleService.findUserRole(UserRoleEnum.ADMIN);
+      UserRole userRole = this.roleService.findUserRole(UserRoleEnum.USER);
+
+      User admin = createUser("admin", "admin", "adminov", "noUrl", true, "test");
+      admin.setRoles(List.of(adminRole, userRole));
+
+      User user = createUser("pesho", "Petar", "Ivanov", "n/a", true, "123");
+      user.setRoles(List.of(userRole));
+
+      this.userRepository.saveAll(List.of(admin, user));
+    }
+  }
+
+  // Helpers
+  private User createUser(String username, String firstName, String lastName, String imageUrl, boolean isActive, String password) {
+    User user = new User();
+    user.setActive(isActive);
+    user.setUsername(username);
+    user.setImageUrl(imageUrl);
+    user.setFirstName(firstName);
+    user.setLastName(lastName);
+    user.setPassword(passwordEncoder.encode(password));
+
+    return user;
   }
 }
