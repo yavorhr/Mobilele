1b5b55076e289d187aa9620159a22e10c4c08962
wolfie <yavorhristozov@gmail.com>
2024-09-23 13:38:06 +0300

    Update DBInit.java
    add services for init and persist data in the db :
    - OfferService offerService
    - BrandService brandService;
    - ModelService modelService;

--- src/main/java/com/example/mobilele/init/DBInit.java
+++ src/main/java/com/example/mobilele/init/DBInit.java
@@ -1,7 +1,6 @@
 package com.example.mobilele.init;
 
-import com.example.mobilele.service.UserRoleService;
-import com.example.mobilele.service.UserService;
+import com.example.mobilele.service.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Service;
 
@@ -9,15 +8,24 @@ import org.springframework.stereotype.Service;
 public class DBInit implements CommandLineRunner {
   private final UserService userService;
   private final UserRoleService  userRoleService;
+  private final OfferService offerService;
+  private final BrandService brandService;
+  private final ModelService modelService;
 
-  public DBInit(UserService userService, UserRoleService userRoleService) {
+  public DBInit(UserService userService, UserRoleService userRoleService, OfferService offerService, BrandService brandService, ModelService modelService) {
     this.userService = userService;
     this.userRoleService = userRoleService;
+    this.offerService = offerService;
+
+    this.brandService = brandService;
+    this.modelService = modelService;
   }
 
   @Override
   public void run(String... args) throws Exception {
     userRoleService.initRoles();
     userService.initUsers();
+    brandService.initBrands();
+    modelService.initModels();
   }
 }
