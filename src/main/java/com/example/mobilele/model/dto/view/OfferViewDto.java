de4072a82de90de1fc885740b35a3b2045f8d475
wolfie <yavorhristozov@gmail.com>
2024-09-23 13:38:17 +0300

    Create OfferViewDto.java

---
+++ src/main/java/com/example/mobilele/model/dto/view/OfferViewDto.java
@@ -0,0 +1,56 @@
+package com.example.mobilele.model.dto.view;
+
+import com.example.mobilele.model.entity.Model;
+import com.example.mobilele.model.entity.User;
+import com.example.mobilele.model.entity.enums.EngineEnum;
+import com.example.mobilele.model.entity.enums.TransmissionType;
+
+public class OfferViewDto {
+  private String description;
+  private EngineEnum engine;
+  private String imageUrl;
+  private Double mileage;
+  private TransmissionType transmission;
+  private Integer year;
+  private Model model;
+  private String brandName;
+
+  public OfferViewDto() {
+  }
+
+  public String getDescription() {
+    return description;
+  }
+
+  public EngineEnum getEngine() {
+    return engine;
+  }
+
+  public String getImageUrl() {
+    return imageUrl;
+  }
+
+  public Double getMileage() {
+    return mileage;
+  }
+
+  public TransmissionType getTransmission() {
+    return transmission;
+  }
+
+  public Integer getYear() {
+    return year;
+  }
+
+  public Model getModel() {
+    return model;
+  }
+
+  public String getBrandName() {
+    return brandName;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+}
