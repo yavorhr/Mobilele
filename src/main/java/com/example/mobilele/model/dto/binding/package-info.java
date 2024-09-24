aa154c3446fac637d80e62bbb4332c7d5be316c3
wolfie <yavorhristozov@gmail.com>
2024-09-24 14:28:43 +0300

    Add : package-info

---
+++ src/main/java/com/example/mobilele/model/dto/binding/package-info.java
@@ -0,0 +1,8 @@
+package com.example.mobilele.model.dto.binding;
+
+/**
+ * This package contains binding models.
+ *
+ * We use them in the controllers in order to read
+ * the parameters that come from the client (the browser).
+ */
---
+++ src/main/java/com/example/mobilele/model/entity/package-info.java
@@ -0,0 +1,7 @@
+package com.example.mobilele.model.entity;
+
+/**
+ * This package contains persistent entities.
+ *
+ * They are used to store and access data in the database.
+ */
