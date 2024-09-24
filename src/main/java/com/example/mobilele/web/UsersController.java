5245af0d063a33eeaecf85e14d26c67f92050dc5
wolfie <yavorhristozov@gmail.com>
2024-09-24 11:33:45 +0300

    Create UsersController.java

---
+++ src/main/java/com/example/mobilele/web/UsersController.java
@@ -0,0 +1,54 @@
+package com.example.mobilele.web;
+
+import com.example.mobilele.model.dto.binding.UserLoginBindingModel;
+import com.example.mobilele.model.dto.service.UserLoginServiceModel;
+import com.example.mobilele.service.UserService;
+import com.example.mobilele.user.CurrentUser;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class UsersController {
+  private final UserService userService;
+  private final CurrentUser currentUser;
+
+  public UsersController(UserService userService, CurrentUser currentUser) {
+    this.userService = userService;
+    this.currentUser = currentUser;
+  }
+
+  @GetMapping("/users/login")
+  public String loginPage() {
+    return "auth-login";
+  }
+
+  @PostMapping("/users/login")
+  public String loginUser(@ModelAttribute UserLoginBindingModel userLoginBindingModel) {
+    UserLoginServiceModel userLoginServiceModel = mapToServiceModel(userLoginBindingModel);
+
+    boolean loginSuccessful = this.userService.login(userLoginServiceModel);
+
+    if (loginSuccessful) {
+      return "redirect:/";
+    }
+
+    return "auth-login";
+  }
+
+  @GetMapping("/users/logout")
+  public String logout() {
+    this.userService.logout();
+    return "redirect:/";
+  }
+
+  // helpers
+
+  private UserLoginServiceModel mapToServiceModel(UserLoginBindingModel userLoginBindingModel) {
+    UserLoginServiceModel userLoginServiceModel = new UserLoginServiceModel();
+    userLoginServiceModel.setUsername(userLoginBindingModel.getUsername());
+    userLoginServiceModel.setPassword(userLoginBindingModel.getPassword());
+    return userLoginServiceModel;
+  }
+}
