c98c37712d2427828be3dc2617aac8f0ade91369
wolfie <yavorhristozov@gmail.com>
2024-09-24 14:28:55 +0300

    Add : package-info

---
+++ src/main/java/com/example/mobilele/config/package-info.java
@@ -0,0 +1,8 @@
+package com.example.mobilele.config;
+
+/**
+ * config package consists of ApplicationBeanConfig class.
+ *
+ * The class is annotated with @Configuration and @Component
+ * It is used to create @Beans, which will be added in the Spring context container (IoC container).
+ */
---
+++ src/main/java/com/example/mobilele/init/package-info.java
@@ -0,0 +1,11 @@
+package com.example.mobilele.init;
+
+/**
+ * init project contains DBInit class.
+ *
+ * The class implements CommandLineRunner interface
+ * DBInit calls the service classes, which all have init() methods
+ * for creating instances and persisting data in the database.
+ *
+ * The class is used for demo tests, until all features of the application are developed.
+ */
