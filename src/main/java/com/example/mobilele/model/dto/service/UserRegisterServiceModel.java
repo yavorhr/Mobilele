1e52545c632222938a3a28191386938aa2af0257
wolfie <yavorhristozov@gmail.com>
2024-09-24 15:59:51 +0300

    Create UserRegisterServiceModel.java

---
+++ src/main/java/com/example/mobilele/model/dto/service/UserRegisterServiceModel.java
@@ -0,0 +1,43 @@
+package com.example.mobilele.model.dto.service;
+
+public class UserRegisterServiceModel {
+  private String username;
+  private String firstName;
+  private String lastName;
+  private String password;
+
+  public UserRegisterServiceModel() {
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public String getFirstName() {
+    return firstName;
+  }
+
+  public String getLastName() {
+    return lastName;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public void setFirstName(String firstName) {
+    this.firstName = firstName;
+  }
+
+  public void setLastName(String lastName) {
+    this.lastName = lastName;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
+}
