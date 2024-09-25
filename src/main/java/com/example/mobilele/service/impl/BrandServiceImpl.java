e2ce3ac493bfb1816ae3bc6e15a8647cb70c00ee
wolfie <yavorhristozov@gmail.com>
2024-09-25 12:22:18 +0300

    Update BrandServiceImpl.java
    - Add init() method for creating and persisting Brand object to "brands" table
    - The init() is invoked only if there are no brands in the database

--- src/main/java/com/example/mobilele/service/impl/BrandServiceImpl.java
+++ src/main/java/com/example/mobilele/service/impl/BrandServiceImpl.java
@@ -1,8 +1,25 @@
 package com.example.mobilele.service.impl;
 
+import com.example.mobilele.model.entity.Brand;
+import com.example.mobilele.repository.BrandRepository;
 import com.example.mobilele.service.BrandService;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BrandServiceImpl implements BrandService {
+  private final BrandRepository brandRepository;
+
+  public BrandServiceImpl(BrandRepository brandRepository) {
+    this.brandRepository = brandRepository;
+  }
+
+  @Override
+  public void initBrands() {
+    if (brandRepository.count() == 0) {
+      Brand bmw = new Brand();
+      bmw.setName("bmw");
+
+      this.brandRepository.save(bmw);
+    }
+  }
 }
