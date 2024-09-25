7778b155d01b1cb5993ad4a5d4fa8c1078a4df50
wolfie <yavorhristozov@gmail.com>
2024-09-25 13:30:32 +0300

    Update ModelService.java
    Add Optional<ModelEntity> findById(Long id)

--- src/main/java/com/example/mobilele/service/ModelService.java
+++ src/main/java/com/example/mobilele/service/ModelService.java
@@ -1,5 +1,11 @@
 package com.example.mobilele.service;
 
+import com.example.mobilele.model.entity.ModelEntity;
+
+import java.util.Optional;
+
 public interface ModelService {
   void initModels();
+
+  Optional<ModelEntity> findById(Long id);
 }
