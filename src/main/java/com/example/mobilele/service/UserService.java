03d868c87492b85fdbd7f1a0150eb90d0e0ade62
wolfie <yavorhristozov@gmail.com>
2024-09-25 13:30:13 +0300

    Update UserService.java
    Add findByUsername(String username);

--- src/main/java/com/example/mobilele/service/UserService.java
+++ src/main/java/com/example/mobilele/service/UserService.java
@@ -1,14 +1,20 @@
 package com.example.mobilele.service;
+
 import com.example.mobilele.model.dto.service.UserLoginServiceModel;
 import com.example.mobilele.model.dto.service.UserRegisterServiceModel;
+import com.example.mobilele.model.entity.User;
+
+import java.util.Optional;
 
 public interface UserService {
 
-  boolean login (UserLoginServiceModel userLoginServiceModel);
+  boolean login(UserLoginServiceModel userLoginServiceModel);
 
   void logout();
 
   void initUsers();
 
   void registerAndLoginUser(UserRegisterServiceModel serviceModel);
+
+  Optional<User> findByUsername(String username);
 }
