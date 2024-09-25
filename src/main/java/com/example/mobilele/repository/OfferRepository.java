33a4ea0a6aa70998033810d0bfafd0af6f4fadd9
wolfie <yavorhristozov@gmail.com>
2024-09-25 11:08:07 +0300

    Update OfferRepository.java

--- src/main/java/com/example/mobilele/repository/OfferRepository.java
+++ src/main/java/com/example/mobilele/repository/OfferRepository.java
@@ -1,9 +1,11 @@
 package com.example.mobilele.repository;
-
 import com.example.mobilele.model.entity.Offer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OfferRepository extends JpaRepository<Offer,Long> {
+
 }
