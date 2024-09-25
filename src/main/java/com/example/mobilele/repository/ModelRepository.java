2609c0addf3894e8d235c2df0eda2485c9b7cea7
wolfie <yavorhristozov@gmail.com>
2024-09-25 12:56:33 +0300

    Update ModelRepository.java

--- src/main/java/com/example/mobilele/repository/ModelRepository.java
+++ src/main/java/com/example/mobilele/repository/ModelRepository.java
@@ -1,9 +1,9 @@
 package com.example.mobilele.repository;
 
-import com.example.mobilele.model.entity.Model;
+import com.example.mobilele.model.entity.ModelEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ModelRepository extends JpaRepository<Model,Long> {
+public interface ModelRepository extends JpaRepository<ModelEntity,Long> {
 }
