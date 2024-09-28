4f34aa03cc7a056c2b161e3587c21f56cfe5d16f
wolfie <yavorhristozov@gmail.com>
2024-09-28 14:58:32 +0300

    Update OfferService.java
    add findAllOffers() signature

--- src/main/java/com/example/mobilele/service/OfferService.java
+++ src/main/java/com/example/mobilele/service/OfferService.java
@@ -1,6 +1,12 @@
 package com.example.mobilele.service;
 
+import com.example.mobilele.model.dto.view.OfferViewModel;
+
+import java.util.List;
+
 public interface OfferService {
 
   void initOffers();
+
+  List<OfferViewModel> findAllOffers();
 }
