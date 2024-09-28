b1b5712260af1aedf22979a8d9ed58cc5ed588b1
wolfie <yavorhristozov@gmail.com>
2024-09-28 14:56:28 +0300

    Update OffersController.java
    - Add @GetMapping("/offers/all"), which returns "offers" view
    - get list of OfferViewModel from OfferService and return it as Model with the "offers" view

--- src/main/java/com/example/mobilele/web/OffersController.java
+++ src/main/java/com/example/mobilele/web/OffersController.java
@@ -1,19 +1,31 @@
 package com.example.mobilele.web;
 
+import com.example.mobilele.model.dto.view.OfferViewModel;
+import com.example.mobilele.service.OfferService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
 
 @Controller
 public class OffersController {
+  private final OfferService offerService;
 
+  public OffersController(OfferService offerService) {
+    this.offerService = offerService;
+  }
 
   @GetMapping("/offers/all")
-  public String getAllOffersPage() {
+  public String getAllOffersPage(Model model) {
+    List<OfferViewModel> offers = this.offerService.findAllOffers();
+    model.addAttribute("offers", offers);
+    
     return "offers";
   }
 
   @GetMapping("/offers/add")
-  public String getAddOffersPage(){
+  public String getAddOffersPage() {
     return "offer-add";
   }
 }
