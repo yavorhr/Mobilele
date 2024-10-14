56f00f156acdbb8133dc652d832d742d96187441
wolfie <yavorhristozov@gmail.com>
2024-10-14 16:06:09 +0300

    Update BrandService.java

--- src/main/java/com/example/mobilele/service/BrandService.java
+++ src/main/java/com/example/mobilele/service/BrandService.java
@@ -1,11 +1,11 @@
 package com.example.mobilele.service;
 
 import com.example.mobilele.model.entity.BrandEntity;
-
 import java.util.Optional;
 
 public interface BrandService {
   void initBrands();
 
   Optional<BrandEntity> findBrandByName(String name);
+
 }
