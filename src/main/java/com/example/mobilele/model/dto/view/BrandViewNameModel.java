65fe3e360363daeb5e1551f34d7b58b0662cbc23
wolfie <yavorhristozov@gmail.com>
2024-10-14 16:35:30 +0300

    Create BrandViewNameModel.java

---
+++ src/main/java/com/example/mobilele/model/dto/view/BrandViewNameModel.java
@@ -0,0 +1,17 @@
+package com.example.mobilele.model.dto.view;
+
+public class BrandViewNameModel {
+  private String name;
+
+  public BrandViewNameModel() {
+  }
+
+  public String getName() {
+    return  name.substring(0,1).toUpperCase() + name.substring(1);
+  }
+
+  public BrandViewNameModel setName(String name) {
+    this.name = name;
+    return this;
+  }
+}
