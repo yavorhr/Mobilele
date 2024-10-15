041290a417aab3276e4aa780c07f9b69c71eb314
wolfie <yavorhristozov@gmail.com>
2024-10-15 12:31:00 +0300

    Update OffersController.java
    Add @ModelAttribute("offerBindingModel")
    Add addOffer with @PostMapping("/offers/add")

--- src/main/java/com/example/mobilele/web/OffersController.java
+++ src/main/java/com/example/mobilele/web/OffersController.java
@@ -1,21 +1,28 @@
 package com.example.mobilele.web;
 
+import com.example.mobilele.model.dto.binding.offer.OfferBindingModel;
 import com.example.mobilele.model.dto.view.OfferViewModel;
+import com.example.mobilele.model.entity.enums.EngineEnum;
+import com.example.mobilele.model.entity.enums.TransmissionType;
+import com.example.mobilele.service.BrandService;
 import com.example.mobilele.service.OfferService;
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import java.util.List;
 
 @Controller
 public class OffersController {
   private final OfferService offerService;
+  private final BrandService brandService;
 
-  public OffersController(OfferService offerService) {
+  public OffersController(OfferService offerService, BrandService brandService) {
     this.offerService = offerService;
+    this.brandService = brandService;
   }
 
   // GET ALL OFFERS
@@ -49,9 +56,36 @@ public class OffersController {
   }
 
   // ADD OFFER
+
+  @ModelAttribute("offerBindingModel")
+  public OfferBindingModel offerBindingModel() {
+    return new OfferBindingModel();
+  }
+
   @GetMapping("/offers/add")
-  public String getAddOffersPage() {
+  public String getAddOffersPage(Model model) {
+    model.addAttribute("brands", this.brandService.findAllBrands());
+    model.addAttribute("engines", EngineEnum.values());
+    model.addAttribute("transmissions", TransmissionType.values());
+
     return "offer-add";
+  }
+
+  @PostMapping("/offers/add")
+  public String addOffer(@Valid OfferBindingModel offerBindingModel,
+                         BindingResult bindingResult,
+                         RedirectAttributes redirectAttributes) {
+
+    if (bindingResult.hasErrors()) {
+      redirectAttributes
+              .addFlashAttribute("offerBindingModel", offerBindingModel)
+              .addFlashAttribute("org.springframework.validation.BindingResult.offerBindingModel", bindingResult);
+
+      return "redirect:/offers/add";
+    }
+    System.out.println();
+    return "";
+
   }
 
   // DELETE OFFER
