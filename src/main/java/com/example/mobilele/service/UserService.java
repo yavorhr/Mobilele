b539ce36fdfb46625a8dba1558978d1c23a3ad16
wolfie <yavorhristozov@gmail.com>
2024-10-16 14:51:29 +0300

    Update UserService.java

--- src/main/java/com/example/mobilele/service/UserService.java
+++ src/main/java/com/example/mobilele/service/UserService.java
@@ -17,4 +17,6 @@ public interface UserService {
   void registerAndLoginUser(UserRegisterServiceModel serviceModel);
 
   Optional<UserEntity> findByUsername(String username);
+
+  UserEntity findById(Long id);
 }
