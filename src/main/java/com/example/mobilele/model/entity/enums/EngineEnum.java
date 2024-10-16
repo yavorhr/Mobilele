e3af267e16e5b221fdec1e1620d7bcce99a90cd6
wolfie <yavorhristozov@gmail.com>
2024-10-16 15:02:24 +0300

    Refact : EngineEnum
    Add private constructor and String parameters for the EngineTypes

--- src/main/java/com/example/mobilele/model/entity/enums/EngineEnum.java
+++ src/main/java/com/example/mobilele/model/entity/enums/EngineEnum.java
@@ -1,5 +1,18 @@
 package com.example.mobilele.model.entity.enums;
 
 public enum EngineEnum {
-  GASOLINE, DIESEL, ELECTRIC, HYBRID
+  GASOLINE("Gasoline"),
+  DIESEL("Diesel"),
+  ELECTRIC("Electric"),
+  HYBRID("Hybrid");
+
+  private final String engineType;
+
+  EngineEnum(String engineType) {
+    this.engineType = engineType;
+  }
+
+  public String getEngineType() {
+    return engineType;
+  }
 }
