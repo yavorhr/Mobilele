46e9c5e033d572e332d51adfcf614e35a3bef4b0
wolfie <yavorhristozov@gmail.com>
2024-10-16 15:20:56 +0300

    Refact : remove the constructor and parameter values

--- src/main/java/com/example/mobilele/model/entity/enums/EngineEnum.java
+++ src/main/java/com/example/mobilele/model/entity/enums/EngineEnum.java
@@ -1,18 +1,8 @@
 package com.example.mobilele.model.entity.enums;
 
 public enum EngineEnum {
-  GASOLINE("Gasoline"),
-  DIESEL("Diesel"),
-  ELECTRIC("Electric"),
-  HYBRID("Hybrid");
-
-  private final String engineType;
-
-  EngineEnum(String engineType) {
-    this.engineType = engineType;
-  }
-
-  public String getEngineType() {
-    return engineType;
-  }
+  Gasoline,
+  Diesel,
+  Electric,
+  Hybrid
 }
