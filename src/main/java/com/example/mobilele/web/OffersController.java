68ef97269771bd59d861f5df9a16b9c7db49c445
wolfie <yavorhristozov@gmail.com>
2024-10-17 14:16:24 +0300

    Update OffersController.java
    Comment  .addFlashAttribute("brands", this.brandService.findAllBrands()) as it is not needed

--- src/main/java/com/example/mobilele/web/OffersController.java
+++ src/main/java/com/example/mobilele/web/OffersController.java
@@ -86,8 +86,8 @@ public class OffersController {
     if (bindingResult.hasErrors()) {
       redirectAttributes
               .addFlashAttribute("offerBindingModel", offerBindingModel)
-              .addFlashAttribute("org.springframework.validation.BindingResult.offerBindingModel", bindingResult)
-              .addFlashAttribute("brands", this.brandService.findAllBrands());
+              .addFlashAttribute("org.springframework.validation.BindingResult.offerBindingModel", bindingResult);
+//              .addFlashAttribute("brands", this.brandService.findAllBrands());
       return "redirect:/offers/add";
     }
 
