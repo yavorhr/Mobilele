3279f75a2fe57b77db9d3ba7de43bbd928629144
wolfie <yavorhristozov@gmail.com>
2024-10-18 10:49:14 +0300

    Update OfferRepository.java
    add  findAllByModel_BrandName(String brandName)

--- src/main/java/com/example/mobilele/repository/OfferRepository.java
+++ src/main/java/com/example/mobilele/repository/OfferRepository.java
@@ -3,7 +3,10 @@ import com.example.mobilele.model.entity.OfferEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface OfferRepository extends JpaRepository<OfferEntity,Long> {
 
+  List<OfferEntity> findAllByModel_BrandName(String brandName);
 }
