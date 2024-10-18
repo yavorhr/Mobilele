089c8d55e8b9bdb16da42177eba34fb0a458789c
wolfie <yavorhristozov@gmail.com>
2024-10-18 14:50:44 +0300

    Update OffersController.java
    1. Add @ModelAttributes for Engines and Transmissions. Remove the passing attributes from the Mapping methods.
    2. Add updateOffer() implementation

--- src/main/java/com/example/mobilele/web/OffersController.java
+++ src/main/java/com/example/mobilele/web/OffersController.java
@@ -1,12 +1,13 @@
 package com.example.mobilele.web;
 
-import com.example.mobilele.model.dto.binding.offer.OfferBindingModel;
+import com.example.mobilele.model.dto.binding.offer.OfferAddBindingModel;
 import com.example.mobilele.model.dto.service.offer.OfferAddServiceModel;
-import com.example.mobilele.model.dto.view.brand.BrandViewModel;
+import com.example.mobilele.model.dto.service.offer.OfferUpdateServiceModel;
 import com.example.mobilele.model.dto.view.offer.OfferViewModel;
 import com.example.mobilele.model.entity.enums.EngineEnum;
 import com.example.mobilele.model.entity.enums.TransmissionType;
 import com.example.mobilele.service.BrandService;
+import com.example.mobilele.service.ModelService;
 import com.example.mobilele.service.OfferService;
 import com.example.mobilele.user.CurrentUser;
 import jakarta.validation.Valid;
@@ -16,6 +17,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,12 +27,29 @@ public class OffersController {
   private final BrandService brandService;
   private final ModelMapper modelMapper;
   private final CurrentUser currentUser;
+  private final ModelService modelService;
 
-  public OffersController(OfferService offerService, BrandService brandService, ModelMapper modelMapper, CurrentUser currentUser) {
+  public OffersController(OfferService offerService, BrandService brandService, ModelMapper modelMapper, CurrentUser currentUser, ModelService modelService) {
     this.offerService = offerService;
     this.brandService = brandService;
     this.modelMapper = modelMapper;
     this.currentUser = currentUser;
+    this.modelService = modelService;
+  }
+
+  @ModelAttribute("offerBindingModel")
+  public OfferAddBindingModel offerBindingModel() {
+    return new OfferAddBindingModel();
+  }
+
+  @ModelAttribute("engines")
+  public EngineEnum[] getEngines() {
+    return EngineEnum.values();
+  }
+
+  @ModelAttribute("transmissions")
+  public TransmissionType[] getTransmissions() {
+    return TransmissionType.values();
   }
 
   // GET
@@ -43,7 +62,7 @@ public class OffersController {
   }
 
   @GetMapping("/offers")
-  public String getModelsByBrandName(@RequestParam String brand, Model model){
+  public String getModelsByBrandName(@RequestParam String brand, Model model) {
     List<OfferViewModel> offersByBrand = offerService
             .findOffersByBrand(brand.toLowerCase())
             .stream()
@@ -57,7 +76,7 @@ public class OffersController {
 
   @GetMapping("/offers/details/{id}")
   public String getOffersDetailsPage(@PathVariable Long id, Model model) {
-    OfferViewModel viewModel = this.offerService.findOfferById(id);
+    OfferViewModel viewModel = this.modelMapper.map(this.offerService.findOfferById(id), OfferViewModel.class);
 
     model.addAttribute("offer", viewModel);
 
@@ -67,36 +86,51 @@ public class OffersController {
   // * UPDATE OFFER
   @GetMapping("/offers/update/{id}")
   public String getOfferUpdatePage(@PathVariable Long id, Model model) {
-    OfferViewModel offerModel = this.offerService.findOfferById(id);
+    OfferAddBindingModel offerBindingModel =
+            this.modelMapper.map(this.offerService.findOfferById(id), OfferAddBindingModel.class);
 
-    model.addAttribute("offer", offerModel);
+    model.addAttribute("models", this.modelService.findModelsPerBrand(offerBindingModel.getBrand()));
+    model.addAttribute("offerBindingModel", offerBindingModel);
 
     return "update";
   }
 
-  // ADD OFFER
-  @ModelAttribute("offerBindingModel")
-  public OfferBindingModel offerBindingModel() {
-    return new OfferBindingModel();
+  @PatchMapping("/offers/update/{id}")
+  public String updateOffer(@PathVariable Long id,
+                            @Valid OfferAddBindingModel offerAddBindingModel,
+                            BindingResult bindingResult,
+                            RedirectAttributes redirectAttributes) {
+
+    if (bindingResult.hasErrors()) {
+      redirectAttributes
+              .addFlashAttribute("offerBindingModel", offerAddBindingModel)
+              .addFlashAttribute("org.springframework.validation.BindingResult.offerBindingModel", bindingResult);
+      return "redirect:/offers/update/" + id;
+    }
+
+    this.offerService.updateOffer(this.modelMapper.map(offerAddBindingModel, OfferUpdateServiceModel.class), currentUser.getId());
+
+    return "redirect:/offers/details/" + id;
   }
 
+  // ADD OFFER
   @GetMapping("/offers/add")
   public String getAddOffersPage(Model model) {
     model.addAttribute("brands", this.brandService.findAllBrands());
-    model.addAttribute("engines", EngineEnum.values());
-    model.addAttribute("transmissions", TransmissionType.values());
+//    model.addAttribute("engines", EngineEnum.values());
+//    model.addAttribute("transmissions", TransmissionType.values());
 
     return "offer-add";
   }
 
   @PostMapping("/offers/add")
-  public String addOffer(@Valid OfferBindingModel offerBindingModel,
+  public String addOffer(@Valid OfferAddBindingModel offerAddBindingModel,
                          BindingResult bindingResult,
                          RedirectAttributes redirectAttributes) {
 
     if (bindingResult.hasErrors()) {
       redirectAttributes
-              .addFlashAttribute("offerBindingModel", offerBindingModel)
+              .addFlashAttribute("offerBindingModel", offerAddBindingModel)
               .addFlashAttribute("org.springframework.validation.BindingResult.offerBindingModel", bindingResult);
 //              .addFlashAttribute("brands", this.brandService.findAllBrands());
       return "redirect:/offers/add";
@@ -104,7 +138,7 @@ public class OffersController {
 
     OfferAddServiceModel serviceModel =
             this.offerService.addOffer(
-                    this.modelMapper.map(offerBindingModel, OfferAddServiceModel.class),
+                    this.modelMapper.map(offerAddBindingModel, OfferAddServiceModel.class),
                     currentUser.getId());
 
     return "redirect:/offers/details/" + serviceModel.getId();
@@ -117,5 +151,4 @@ public class OffersController {
 
     return "redirect:/offers/all";
   }
-
 }
