65cbbf5557073ca882de73d349a8ac625690c822
wolfie <yavorhristozov@gmail.com>
2024-11-22 15:47:25 +0200

    Create UserLoginController.java
    move the Login action method in separate Controller

---
+++ src/main/java/com/example/mobilele/web/UserLoginController.java
@@ -0,0 +1,27 @@
+package com.example.mobilele.web;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+@Controller
+public class UserLoginController {
+
+  @GetMapping("/users/login")
+  public String login() {
+    return "auth-login";
+  }
+
+  @PostMapping("/users/login-error")
+  public String failedLogin(@ModelAttribute(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY) String username,
+                            RedirectAttributes attributes) {
+
+    attributes.addFlashAttribute("bad_credentials", true);
+    attributes.addFlashAttribute("username", username);
+
+    return "redirect:/users/login";
+  }
+}
