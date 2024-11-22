cb0d96801809588adbf2bec0a6815777e0a81450
wolfie <yavorhristozov@gmail.com>
2024-11-22 15:47:58 +0200

    Update ApplicationSecurityConfiguration.java
    remove comments

--- src/main/java/com/example/mobilele/config/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/mobilele/config/ApplicationSecurityConfiguration.java
@@ -28,42 +28,24 @@ public class ApplicationSecurityConfiguration {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http
-            .authorizeHttpRequests(authz ->
-                    authz.
-                            // with this line we allow access to all static resources
-                                    requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll().
-                            // allow actuator endpoints
-                                    requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll().
-                            // the next line allows access to the home page, login page and registration for everyone
-                                    requestMatchers("/", "/users/login", "/users/register", "/logger/**").permitAll().
-                            // we permit the page below only for admin users
-                                    requestMatchers("/statistics").hasRole(UserRoleEnum.ADMIN.name()).
-                            // next we forbid all other pages for unauthenticated users.
-                                    requestMatchers("/**").authenticated()).
-            // configure login with login HTML form with two input fields
-                    formLogin(form -> form.
-                    // our login page is located at http://<serveraddress>>:<port>/users/login
-                            loginPage("/users/login").
-                    // this is the name of the <input..> in the login form where the user enters her email/username/etc
-                    // the value of this input will be presented to our User details service
-                    // those that want to name the input field differently, e.g. email may change the value below
-                            usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
-                    // the name of the <input...> HTML filed that keeps the password
-                            passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
-                    // The place where we should land in case that the login is successful
-                            defaultSuccessUrl("/", true).
-                    // the place where I should land if the login is NOT successful
-                            failureForwardUrl("/users/login-error").permitAll()).
-            logout(logout -> logout.
-                    // This is the URL which spring will implement for me and will log the user out.
-                            logoutUrl("/users/logout").
-                    // where to go after the logout.
-                            logoutSuccessUrl("/").
-                    // remove the session from server
-                            invalidateHttpSession(true).
-                    //delete the cookie that references my session
-                            deleteCookies("JSESSIONID"));
+    http.authorizeHttpRequests(authz ->
+            authz.
+                    requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll().
+                    requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll().
+                    requestMatchers("/", "/users/login", "/users/register", "/logger/**").permitAll().
+                    requestMatchers("/statistics").hasRole(UserRoleEnum.ADMIN.name()).
+                    requestMatchers("/**").authenticated())
+            .formLogin(form -> form.
+                    loginPage("/users/login").
+                    usernameParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_USERNAME_KEY).
+                    passwordParameter(UsernamePasswordAuthenticationFilter.SPRING_SECURITY_FORM_PASSWORD_KEY).
+                    defaultSuccessUrl("/", true).
+                    failureForwardUrl("/login?fail=true"))
+            .logout(logout -> logout.
+                    logoutUrl("/users/logout").
+                    logoutSuccessUrl("/").
+                    invalidateHttpSession(true).
+                    deleteCookies("JSESSIONID"));
 
     return http.build();
   }
