28a23f18b330235ac02bb7e00f1b8d97ccf98fd3
wolfie <yavorhristozov@gmail.com>
2024-12-04 17:12:21 +0200

    Update ApplicationSecurityConfiguration.java
    @EnableMethodSecurity(prePostEnabled = true)

--- src/main/java/com/example/mobilele/config/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/mobilele/config/ApplicationSecurityConfiguration.java
@@ -8,6 +8,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -16,7 +18,7 @@ import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
-@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 public class ApplicationSecurityConfiguration {
   private final UserDetailsService userDetailsService;
   private final PasswordEncoder passwordEncoder;
