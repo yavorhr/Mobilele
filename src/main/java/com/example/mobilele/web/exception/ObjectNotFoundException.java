191178cb03f3fb65444f3862a478186bc0023d43
wolfie <yavorhristozov@gmail.com>
2024-12-04 16:32:49 +0200

    Update ObjectNotFoundException.java
    Custom exception for missing resources

--- src/main/java/com/example/mobilele/web/exception/ObjectNotFoundException.java
+++ src/main/java/com/example/mobilele/web/exception/ObjectNotFoundException.java
@@ -1,10 +1,8 @@
 package com.example.mobilele.web.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
-@ResponseStatus(HttpStatus.NOT_FOUND)
+//@ResponseStatus(HttpStatus.NOT_FOUND)
 public class ObjectNotFoundException extends RuntimeException {
+
   public ObjectNotFoundException(String message) {
     super(message);
   }
