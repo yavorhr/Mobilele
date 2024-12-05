a7e3724ecffbc131e04bb5c9c8b931d61ae45aff
wolfie <yavorhristozov@gmail.com>
2024-12-05 16:28:08 +0200

    Refact : rename global handler

--- src/main/java/com/example/mobilele/web/exception/ObjectNotFoundGlobalExceptionHandler.java
+++ src/main/java/com/example/mobilele/web/exception/ObjectNotFoundGlobalExceptionHandler.java
@@ -1,18 +1,19 @@
 package com.example.mobilele.web.exception;
 
+import org.hibernate.ObjectNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 @ControllerAdvice
-public class ResourceNotFoundGlobalException {
+public class ObjectNotFoundGlobalExceptionHandler {
   @ExceptionHandler(ObjectNotFoundException.class)
   public ModelAndView handleObjectNotFoundException(ObjectNotFoundException e) {
     ModelAndView modelAndView = new ModelAndView();
 
     modelAndView.addObject("message", e.getMessage());
-    modelAndView.setViewName("error/not-found");
+    modelAndView.setViewName("error/404");
     modelAndView.setStatus(HttpStatus.NOT_FOUND);
 
     return modelAndView;
