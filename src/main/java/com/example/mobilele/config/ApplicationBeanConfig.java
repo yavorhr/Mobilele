2d79a12ad6b510a983982371e30a5dcc7ee2e5a8
wolfie <yavorhristozov@gmail.com>
2024-12-05 11:39:33 +0200

    Update ApplicationBeanConfig.java

--- src/main/java/com/example/mobilele/config/ApplicationBeanConfig.java
+++ src/main/java/com/example/mobilele/config/ApplicationBeanConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
-import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 
 @Configuration
 public class ApplicationBeanConfig {
