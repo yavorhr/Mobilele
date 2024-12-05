dc9a6e7850d4fd655464653733369ee961e891bc
wolfie <yavorhristozov@gmail.com>
2024-12-05 11:39:19 +0200

    Update WebConfiguration.java
    Add path to static resources, so Spring would be able to find the /static/error folder

--- src/main/java/com/example/mobilele/config/WebConfiguration.java
+++ src/main/java/com/example/mobilele/config/WebConfiguration.java
@@ -3,6 +3,7 @@ package com.example.mobilele.config;
 import com.example.mobilele.web.interceptor.StatsInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -18,4 +19,9 @@ public class WebConfiguration implements WebMvcConfigurer {
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(statsInterceptor);
   }
+
+  @Override
+  public void addResourceHandlers(ResourceHandlerRegistry registry) {
+    registry.addResourceHandler("/static/**")
+            .addResourceLocations("classpath:/static/");  }
 }
