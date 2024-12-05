0d007f0aaf5140f7b9a806688f8c510c388f7014
wolfie <yavorhristozov@gmail.com>
2024-12-05 11:39:28 +0200

    Update ApplicationSecurityConfiguration.java
    permit static resources

--- src/main/java/com/example/mobilele/config/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/mobilele/config/ApplicationSecurityConfiguration.java
@@ -6,12 +6,9 @@ import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -28,7 +25,6 @@ public class ApplicationSecurityConfiguration {
     this.passwordEncoder = passwordEncoder;
   }
 
-
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
     http.authorizeHttpRequests(authz ->
@@ -37,6 +33,7 @@ public class ApplicationSecurityConfiguration {
                     requestMatchers(EndpointRequest.toAnyEndpoint()).permitAll().
                     requestMatchers("/", "/users/login", "/users/register", "/logger/**").permitAll().
                     requestMatchers("/statistics").hasRole(UserRoleEnum.ADMIN.name()).
+                    requestMatchers("/static/**").permitAll().
                     requestMatchers("/**").authenticated())
             .formLogin(form -> form.
                     loginPage("/users/login").
