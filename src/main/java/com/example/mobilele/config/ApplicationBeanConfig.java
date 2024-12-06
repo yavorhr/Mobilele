ff6f271c3a40ad98a66b7875c8da793c15e0922a
wolfie <yavorhristozov@gmail.com>
2024-12-06 13:56:14 +0200

    Update ApplicationBeanConfig.java
    Add i18n @Bean components

--- src/main/java/com/example/mobilele/config/ApplicationBeanConfig.java
+++ src/main/java/com/example/mobilele/config/ApplicationBeanConfig.java
@@ -1,10 +1,19 @@
 package com.example.mobilele.config;
 
+import jakarta.servlet.http.Cookie;
 import org.modelmapper.ModelMapper;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.CookieLocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+
+import java.util.Locale;
+import java.util.TimeZone;
 
 @Configuration
 public class ApplicationBeanConfig {
@@ -19,5 +28,28 @@ public class ApplicationBeanConfig {
     return new ModelMapper();
   }
 
+  @Bean
+  public LocaleResolver localeResolver() {
+    CookieLocaleResolver localeResolver = new CookieLocaleResolver();
+    localeResolver.setDefaultLocale(Locale.ENGLISH);
+    localeResolver.setDefaultTimeZone(TimeZone.getTimeZone("UTC"));
+    return localeResolver;
+  }
+
+
+  @Bean
+  public LocaleChangeInterceptor localeChangeInterceptor() {
+    LocaleChangeInterceptor lci = new LocaleChangeInterceptor();
+    lci.setParamName("lang");
+    return lci;
+  }
+
+  @Bean
+  public MessageSource messageSource() {
+    ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+    messageSource.setBasename("i18n/messages");
+    messageSource.setDefaultEncoding("UTF-8");
+    return messageSource;
+  }
 
 }
