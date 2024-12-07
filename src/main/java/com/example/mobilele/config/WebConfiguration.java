e81fa6d7cd2a0c7f000a2376fd19c3a2a7308371
wolfie <yavorhristozov@gmail.com>
2024-12-07 15:59:15 +0200

    Update WebConfiguration.java
    add changeInterceptor to the registry

--- src/main/java/com/example/mobilele/config/WebConfiguration.java
+++ src/main/java/com/example/mobilele/config/WebConfiguration.java
@@ -5,19 +5,24 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 
 @Configuration
 public class WebConfiguration implements WebMvcConfigurer {
 
   private final StatsInterceptor statsInterceptor;
+  private final LocaleChangeInterceptor changeInterceptor;
 
-  public WebConfiguration(StatsInterceptor statsInterceptor) {
+
+  public WebConfiguration(StatsInterceptor statsInterceptor, LocaleChangeInterceptor changeInterceptor) {
     this.statsInterceptor = statsInterceptor;
+    this.changeInterceptor = changeInterceptor;
   }
 
   @Override
   public void addInterceptors(InterceptorRegistry registry) {
     registry.addInterceptor(statsInterceptor);
+    registry.addInterceptor(changeInterceptor);
   }
 
   @Override
