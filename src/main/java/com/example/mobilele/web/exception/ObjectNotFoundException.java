8410aabf043f30daab4cd0e9608f59766d068f55
wolfie <yavorhristozov@gmail.com>
2025-09-30 15:47:42 +0300

    Update ObjectNotFoundException.java

--- src/main/java/com/example/mobilele/web/exception/ObjectNotFoundException.java
+++ src/main/java/com/example/mobilele/web/exception/ObjectNotFoundException.java
@@ -1,8 +1,10 @@
 package com.example.mobilele.web.exception;
 
-//@ResponseStatus(HttpStatus.NOT_FOUND)
-public class ObjectNotFoundException extends RuntimeException {
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
+@ResponseStatus(HttpStatus.NOT_FOUND)
+public class ObjectNotFoundException extends RuntimeException {
   public ObjectNotFoundException(String message) {
     super(message);
   }
