7f02a36423b484549684e22a6acfa72263076cc5
wolfie <yavorhristozov@gmail.com>
2025-09-30 15:49:35 +0300

    refact : remove empty rows

--- src/main/java/com/example/mobilele/config/ApplicationBeanConfig.java
+++ src/main/java/com/example/mobilele/config/ApplicationBeanConfig.java
@@ -10,7 +10,6 @@ import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.i18n.CookieLocaleResolver;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
-
 import java.util.Locale;
 import java.util.TimeZone;
 
