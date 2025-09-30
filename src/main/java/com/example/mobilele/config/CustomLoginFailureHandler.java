617ee581a472ccd7931dae2560da36e94e71846d
wolfie <yavorhristozov@gmail.com>
2025-09-30 11:58:08 +0300

    Update CustomLoginFailureHandler.java

--- src/main/java/com/example/mobilele/config/CustomLoginFailureHandler.java
+++ src/main/java/com/example/mobilele/config/CustomLoginFailureHandler.java
@@ -1,14 +1,18 @@
 package com.example.mobilele.config;
 
+import com.example.mobilele.model.entity.UserEntity;
 import com.example.mobilele.model.entity.enums.LoginErrorType;
 import com.example.mobilele.service.UserService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.hibernate.ObjectNotFoundException;
+import org.springframework.security.authentication.AccountExpiredException;
+import org.springframework.security.authentication.LockedException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
+
 import java.io.IOException;
 
 @Component
@@ -21,14 +25,21 @@ public class CustomLoginFailureHandler implements AuthenticationFailureHandler {
 
   @Override
   public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
-    String username = request.getParameter("username");
     String errorType = LoginErrorType.INVALID_CREDENTIALS.name();
 
     try {
-      userService.findByUsername(username);
+      UserEntity user = userService.findByUsername("username");
+
+      if (exception instanceof LockedException) {
+        errorType = LoginErrorType.ACCOUNT_LOCKED.name();
+      }
+
+      userService.increaseUserFailedLoginAttempts(user);
+
     } catch (ObjectNotFoundException e) {
       errorType = LoginErrorType.USER_NOT_FOUND.name();
     }
+
     response.sendRedirect("/users/login?errorType=" + errorType);
   }
 }
