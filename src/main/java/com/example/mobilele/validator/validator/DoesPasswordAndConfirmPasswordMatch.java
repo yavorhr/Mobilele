c868f75ce6f6ecdc3582fff6c97fcddac737eba9
wolfie <yavorhristozov@gmail.com>
2025-09-30 16:55:39 +0300

    Create DoesPasswordAndConfirmPasswordMatch.java

---
+++ src/main/java/com/example/mobilele/validator/validator/DoesPasswordAndConfirmPasswordMatch.java
@@ -0,0 +1,20 @@
+package com.example.mobilele.validator.validator;
+
+import jakarta.validation.Constraint;
+import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PasswordsValidator.class)
+public @interface DoesPasswordAndConfirmPasswordMatch {
+  String message() default "Passwords don't match!";
+
+  Class<?>[] groups() default {};
+
+  Class<? extends Payload>[] payload() default {};
+
+}
