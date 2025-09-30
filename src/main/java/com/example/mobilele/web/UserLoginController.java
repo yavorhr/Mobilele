a10a3f2d70005f2e32acdd7e8682c1977450b7d6
wolfie <yavorhristozov@gmail.com>
2025-09-30 14:05:08 +0300

    Update UserLoginController.java
    add error messages

--- src/main/java/com/example/mobilele/web/UserLoginController.java
+++ src/main/java/com/example/mobilele/web/UserLoginController.java
@@ -17,8 +17,9 @@ public class UserLoginController {
         LoginErrorType type = LoginErrorType.valueOf(errorType);
 
         switch (type) {
-          case INVALID_CREDENTIALS -> model.addAttribute("login_error_message", "Invalid credentials. Please try again.");
-          case USER_NOT_FOUND -> model.addAttribute("login_error_message", "User with this username does not exist.");
+          case INVALID_CREDENTIALS -> model.addAttribute("login_error_message", "Invalid username or password");
+          case USER_NOT_FOUND -> model.addAttribute("login_error_message", "User with this username does not exist");
+          case ACCOUNT_LOCKED -> model.addAttribute("login_error_message", "Your account is locked. Try again in 15 minutes");
         }
       } catch (IllegalArgumentException e) {
         model.addAttribute("login_error_message", "An unknown error occurred.");
