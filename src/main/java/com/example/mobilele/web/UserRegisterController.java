ce93a9c79dae318e2e8edbc9fd2af0b958c04e9d
wolfie <yavorhristozov@gmail.com>
2025-10-01 11:24:18 +0300

    Update UserRegisterController.java
    refact register @postMapping

--- src/main/java/com/example/mobilele/web/UserRegisterController.java
+++ src/main/java/com/example/mobilele/web/UserRegisterController.java
@@ -6,13 +6,16 @@ import com.example.mobilele.service.UserService;
 import jakarta.validation.Valid;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
+@RequestMapping("/users")
 public class UserRegisterController {
   private final UserService userService;
   private final ModelMapper modelMapper;
@@ -27,22 +30,24 @@ public class UserRegisterController {
     return new UserRegisterBindingModel();
   }
 
-  @GetMapping("/users/register")
-  public String registerPage() {
-    return "auth-register";
+  @GetMapping("/register")
+  public String registerPage(Model model) {
+    System.out.println(model);
+    return "register";
   }
 
-  @PostMapping("/users/register")
+  @PostMapping("/register")
   public String register(
           @Valid UserRegisterBindingModel userRegisterBindingModel,
           BindingResult bindingResult,
           RedirectAttributes redirectAttributes) {
 
-    if (bindingResult.hasErrors() || !userRegisterBindingModel.getPassword().equals(userRegisterBindingModel.getConfirmPassword())) {
-      redirectAttributes.addFlashAttribute("userRegisterBindingModel", userRegisterBindingModel);
-      redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.userRegisterBindingModel", bindingResult);
+    if (bindingResult.hasErrors()) {
+      redirectAttributes
+              .addFlashAttribute("userRegisterBindingModel", userRegisterBindingModel)
+              .addFlashAttribute("org.springframework.validation.BindingResult.userRegisterBindingModel", bindingResult);
 
-      return "redirect:/users/register";
+      return "redirect:register";
     }
 
     UserRegisterServiceModel serviceModel =
