7a867b2e31c60f5064a10373b161ff46921934bf
wolfie <yavorhristozov@gmail.com>
2025-10-08 13:49:22 +0300

    Create PictureRepository.java

---
+++ src/main/java/com/example/mobilele/repository/PictureRepository.java
@@ -0,0 +1,14 @@
+package com.example.mobilele.repository;
+
+import com.example.mobilele.model.entity.Picture;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PictureRepository extends JpaRepository<Picture, Long> {
+
+//  @Query("SELECT p.url FROM Picture as p")
+//  List<String> getAllPictureUrls();
+//
+//  void deleteByPublicId(String publicId);
+}
