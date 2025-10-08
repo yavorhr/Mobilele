6f52f3c2b022bcc9c1f9ac0b19035dd1ca4b115f
wolfie <yavorhristozov@gmail.com>
2025-10-08 13:48:08 +0300

    Create Picture.java
    add Picture entity

---
+++ src/main/java/com/example/mobilele/model/entity/Picture.java
@@ -0,0 +1,68 @@
+package com.example.mobilele.model.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "offers_pictures")
+public class Picture extends BaseEntity {
+  private String title;
+  private String url;
+  private UserEntity author;
+  private OfferEntity offer;
+  private String publicId;
+
+  public Picture() {
+  }
+
+  public String getPublicId() {
+    return publicId;
+  }
+
+  @ManyToOne
+  public UserEntity getAuthor() {
+    return author;
+  }
+
+  @ManyToOne
+  public OfferEntity getOffer() {
+    return offer;
+  }
+
+  @Column()
+  public String getTitle() {
+    return title;
+  }
+
+  @Column()
+  public String getUrl() {
+    return url;
+  }
+
+  public Picture setTitle(String title) {
+    this.title = title;
+    return this;
+  }
+
+  public Picture setUrl(String url) {
+    this.url = url;
+    return this;
+  }
+
+  public Picture setAuthor(UserEntity author) {
+    this.author = author;
+    return this;
+  }
+
+  public Picture setOffer(OfferEntity offer) {
+    this.offer = offer;
+    return this;
+  }
+
+  public Picture setPublicId(String publicId) {
+    this.publicId = publicId;
+    return this;
+  }
+}
