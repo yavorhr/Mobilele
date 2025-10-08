28005988c8171440a09bfeeeab71276993c2d5fd
wolfie <yavorhristozov@gmail.com>
2025-10-08 11:45:11 +0300

    add : Cloudinary Config classes

---
+++ src/main/java/com/example/mobilele/config/cloudinary/AppConfig.java
@@ -0,0 +1,28 @@
+package com.example.mobilele.config.cloudinary;
+
+import com.cloudinary.Cloudinary;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import java.util.Map;
+
+@Configuration
+@EnableTransactionManagement
+public class AppConfig {
+  private final CloudinaryConfig config;
+
+  public AppConfig(CloudinaryConfig config) {
+    this.config = config;
+  }
+
+  @Bean
+  public Cloudinary cloudinary() {
+    return new Cloudinary(
+            Map.of(
+                    "cloud_name", config.getCloudName(),
+                    "api_key", config.getApiKey(),
+                    "api_secret", config.getApiSecret()
+            )
+    );
+  }
+}
