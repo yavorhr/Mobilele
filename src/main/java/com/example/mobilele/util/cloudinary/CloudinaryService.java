44310e1a522cf9de12f83310f06059964fa9cbbc
wolfie <yavorhristozov@gmail.com>
2025-10-08 11:10:01 +0300

    feat : add Cloudinary service

---
+++ src/main/java/com/example/mobilele/util/cloudinary/CloudinaryService.java
@@ -0,0 +1,13 @@
+package com.example.mobilele.util.cloudinary;
+
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+
+public interface CloudinaryService {
+
+  CloudinaryImage upload(MultipartFile file, String folder) throws IOException;
+
+  boolean delete(String publicId);
+
+}
