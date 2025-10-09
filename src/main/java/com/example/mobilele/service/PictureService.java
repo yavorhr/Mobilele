01c2bb4a913be4eb002cb722b3477390c4b72176
wolfie <yavorhristozov@gmail.com>
2025-10-09 20:12:03 +0300

    refact : PictureService
    add findById(Long id);

--- src/main/java/com/example/mobilele/service/PictureService.java
+++ src/main/java/com/example/mobilele/service/PictureService.java
@@ -1,13 +1,13 @@
 package com.example.mobilele.service;
 
-
 import com.example.mobilele.model.entity.Picture;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.List;
 
 public interface PictureService {
 
   List<Picture> addOfferPictures(List<MultipartFile> pictures) throws IOException;
+
+  Picture findById(Long id);
 }
--- src/main/java/com/example/mobilele/service/impl/PictureServiceImpl.java
+++ src/main/java/com/example/mobilele/service/impl/PictureServiceImpl.java
@@ -1,22 +1,27 @@
 package com.example.mobilele.service.impl;
 
 import com.example.mobilele.model.entity.Picture;
+import com.example.mobilele.repository.PictureRepository;
 import com.example.mobilele.service.PictureService;
+import com.example.mobilele.service.UserService;
 import com.example.mobilele.util.cloudinary.CloudinaryImage;
 import com.example.mobilele.util.cloudinary.CloudinaryService;
+import com.example.mobilele.web.exception.ObjectNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
 public class PictureServiceImpl implements PictureService {
-
   private final CloudinaryService cloudinaryService;
+  private final PictureRepository pictureRepository;
 
-  public PictureServiceImpl(CloudinaryService cloudinaryService) {
+  public PictureServiceImpl(CloudinaryService cloudinaryService, PictureRepository pictureRepository) {
     this.cloudinaryService = cloudinaryService;
+    this.pictureRepository = pictureRepository;
   }
 
   @Override
@@ -33,6 +38,13 @@ public class PictureServiceImpl implements PictureService {
     }
 
     return uploadedPictures;
+  }
+
+  @Override
+  public Picture findById(Long id) {
+    return this.pictureRepository
+            .findById(id)
+            .orElseThrow(() -> new ObjectNotFoundException("Picture with id" + id + "does not exist!"));
   }
 
   // Helpers
