0aa8743a0fded5600f6ae44b0499e8236615528a
wolfie <yavorhristozov@gmail.com>
2025-10-13 12:31:53 +0300

    Update ModelRepository.java
    refact @query

--- src/main/java/com/example/mobilele/repository/ModelRepository.java
+++ src/main/java/com/example/mobilele/repository/ModelRepository.java
@@ -1,10 +1,11 @@
 package com.example.mobilele.repository;
 
 import com.example.mobilele.model.entity.ModelEntity;
+import com.example.mobilele.model.entity.enums.VehicleCategoryEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-import org.springframework.ui.Model;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,8 +13,9 @@ import java.util.Optional;
 @Repository
 public interface ModelRepository extends JpaRepository<ModelEntity, Long> {
 
-  @Query("SELECT m.name FROM ModelEntity m WHERE m.brand.name = :brandName")
-  List<String> findAllByBrandName(String brandName);
+  @Query("SELECT m.name FROM ModelEntity m WHERE m.brand.name = :brandName AND m.vehicleType = :vehicleType")
+  List<String> findAllByBrandNameAndVehicleType(@Param("brandName") String brandName,
+                                                @Param("vehicleType") VehicleCategoryEnum vehicleType);
 
   Optional<ModelEntity> findByName(String name);
 }
