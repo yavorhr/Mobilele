b16edb217f6af2195b4d70208c8381c39a555e16
wolfie <yavorhristozov@gmail.com>
2025-10-13 17:26:13 +0300

    Update OfferRepository.java
    OfferRepository extends JpaSpecificationExecutorJpaSpecificationExecutor for CriteriaBuilder

--- src/main/java/com/example/mobilele/repository/OfferRepository.java
+++ src/main/java/com/example/mobilele/repository/OfferRepository.java
@@ -2,11 +2,12 @@ package com.example.mobilele.repository;
 import com.example.mobilele.model.entity.OfferEntity;
 import com.example.mobilele.model.entity.enums.VehicleCategoryEnum;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 import java.util.List;
 
 @Repository
-public interface OfferRepository extends JpaRepository<OfferEntity,Long> {
+public interface OfferRepository extends JpaRepository<OfferEntity,Long>, JpaSpecificationExecutor<OfferEntity> {
 
   List<OfferEntity> findAllByModel_Brand_NameAndModel_VehicleType(String brandName, VehicleCategoryEnum vehicleType);
 }
