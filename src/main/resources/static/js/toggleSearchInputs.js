206535eb4c62e8741c1e4f3665a01a873b6b9e84
wolfie <yavorhristozov@gmail.com>
2025-10-13 15:59:27 +0300

    Create toggleSearchInputs.js

---
+++ src/main/resources/static/js/toggleSearchInputs.js
@@ -0,0 +1,43 @@
+
+    function toggleMaxInput(field) {
+        const select = document.getElementById(field + 'Comparison');
+        const maxInput = document.getElementById(field + 'Max');
+        const mainInput = document.getElementById(field);
+
+        if (select.value === 'between') {
+            maxInput.style.display = 'inline-block';
+
+            switch (field) {
+                case 'price':
+                    mainInput.placeholder = 'Start Price';
+                    maxInput.placeholder = 'Max Price';
+                    break;
+                case 'mileage':
+                    mainInput.placeholder = 'Min Mileage';
+                    maxInput.placeholder = 'Max Mileage';
+                    break;
+                case 'year':
+                    mainInput.placeholder = 'From Year';
+                    maxInput.placeholder = 'To Year';
+                    break;
+            }
+        } else {
+            maxInput.style.display = 'none';
+
+            switch (field) {
+                case 'price':
+                    mainInput.placeholder = 'Suggested price';
+                    break;
+                case 'mileage':
+                    mainInput.placeholder = 'Mileage in km';
+                    break;
+                case 'year':
+                    mainInput.placeholder = 'Manufacturing year';
+                    break;
+            }
+        }
+
+        ['price', 'mileage', 'year'].forEach(field => toggleMaxInput(field));
+    }
+
+
