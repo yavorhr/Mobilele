8def815a057f281074adc08980d5cd72e023b66e
wolfie <yavorhristozov@gmail.com>
2025-10-16 18:33:15 +0300

    Update OfferServiceImpl.java

--- src/main/java/com/example/mobilele/service/impl/OfferServiceImpl.java
+++ src/main/java/com/example/mobilele/service/impl/OfferServiceImpl.java
@@ -18,7 +18,6 @@ import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
-
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Collection;
