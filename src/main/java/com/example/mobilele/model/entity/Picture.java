d8af6d0dad8afb0438275be175d0eb11b95ec125
wolfie <yavorhristozov@gmail.com>
2025-10-17 17:22:24 +0300

    Update Picture.java
    rename table

--- src/main/java/com/example/mobilele/model/entity/Picture.java
+++ src/main/java/com/example/mobilele/model/entity/Picture.java
@@ -6,7 +6,7 @@ import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "offers_pictures")
+@Table(name = "pictures")
 public class Picture extends BaseEntity {
   private String title;
   private String url;
