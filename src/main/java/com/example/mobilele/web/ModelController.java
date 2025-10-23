174de05904d4ee376b969e82ba1c5feeea23274f
wolfie <yavorhristozov@gmail.com>
2025-10-23 14:30:04 +0300

    Update ModelController.java
    remove parseVehicleCategory

--- src/main/java/com/example/mobilele/web/ModelController.java
+++ src/main/java/com/example/mobilele/web/ModelController.java
@@ -2,13 +2,10 @@ package com.example.mobilele.web;
 
 import com.example.mobilele.model.entity.enums.VehicleCategoryEnum;
 import com.example.mobilele.service.ModelService;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
-
 import java.util.List;
 import java.util.Locale;
 
@@ -25,36 +22,15 @@ public class ModelController {
           @RequestParam String brand,
           @RequestParam String vehicleType) {
 
-    VehicleCategoryEnum category = parseVehicleCategory(vehicleType);
+    VehicleCategoryEnum category = VehicleCategoryEnum.valueOf(vehicleType.toUpperCase(Locale.ROOT));
 
     List<String> models =
             modelService.findModelsByVehicleTypeAndBrand(
-                    brand,
+                    brand.toUpperCase(Locale.ROOT),
                     category);
 
     return ResponseEntity.ok(models);
   }
 
-  // Helpers
-  private VehicleCategoryEnum parseVehicleCategory(String input) {
-    if (input == null || input.isBlank()) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Vehicle type cannot be empty");
-    }
 
-    String vehicle = input.trim().toLowerCase();
-
-    if (vehicle.endsWith("es")) {
-      vehicle = vehicle.substring(0, vehicle.length() - 2);
-    } else if (vehicle.endsWith("s")) {
-      vehicle = vehicle.substring(0, vehicle.length() - 1);
-    }
-
-    vehicle = vehicle.toUpperCase();
-
-    try {
-      return VehicleCategoryEnum.valueOf(vehicle);
-    } catch (IllegalArgumentException ex) {
-      throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Invalid vehicle type: " + input);
-    }
-  }
 }
