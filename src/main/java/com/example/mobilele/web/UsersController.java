add363cd0dc44d940cfc6f6e13e792c3f0a162ae
wolfie <yavorhristozov@gmail.com>
2025-11-10 12:47:53 +0200

    Update UsersController.java

--- src/main/java/com/example/mobilele/web/UsersController.java
+++ src/main/java/com/example/mobilele/web/UsersController.java
@@ -22,7 +22,6 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-
 import java.security.Principal;
 import java.util.HashMap;
 import java.util.Map;
