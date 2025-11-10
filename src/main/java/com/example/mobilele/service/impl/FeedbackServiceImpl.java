169d2f10f726f1948ff1a76adcc9264e3cf45ce4
wolfie <yavorhristozov@gmail.com>
2025-11-10 12:47:11 +0200

    Update FeedbackServiceImpl.java
    init test feedbacks

--- src/main/java/com/example/mobilele/service/impl/FeedbackServiceImpl.java
+++ src/main/java/com/example/mobilele/service/impl/FeedbackServiceImpl.java
@@ -50,4 +50,37 @@ public class FeedbackServiceImpl implements FeedbackService {
             .map(o -> modelMapper.map(o, FeedbackViewModel.class))
             .collect(Collectors.toList());
   }
+
+  @Override
+  public void initFeedbacks() {
+    if (feedbackRepository.count() == 0) {
+
+
+      UserEntity admin = this.userRepository
+              .findByUsername("admin")
+              .orElseThrow(() -> new ObjectNotFoundException("User with username: admin does not exist!"));
+
+      UserEntity user = this.userRepository
+              .findByUsername("user")
+              .orElseThrow(() -> new ObjectNotFoundException("User with username: admin does not exist!"));
+
+      Feedback feedback = createFeedback(admin, "The app is very well made!", 5, Instant.now());
+      Feedback feedback2 = createFeedback(user, "I just don't like it!", 1, Instant.now());
+      Feedback feedback3 = createFeedback(user, "The application is cool, but there are no many offers yet!", 4, Instant.now());
+      Feedback feedback4 = createFeedback(admin, "You rock!", 5, Instant.now());
+      Feedback feedback5 = createFeedback(admin, "Just found my future car here!", 5, Instant.now());
+
+      feedbackRepository.saveAll(List.of(feedback, feedback2, feedback3, feedback4, feedback5));
+    }
+  }
+
+  private Feedback createFeedback(UserEntity userEntity, String comment, int rating, Instant created) {
+    Feedback feedback = new Feedback();
+    feedback.setUser(userEntity);
+    feedback.setComment(comment);
+    feedback.setRating(rating);
+    feedback.setCreated(created);
+
+    return feedback;
+  }
 }
