9a967d35d518554384e571cb4a569cf4de760e24
wolfie <yavorhristozov@gmail.com>
2025-11-14 13:19:14 +0200

    Update AdminServiceImpl.java

--- src/main/java/com/example/mobilele/service/impl/AdminServiceImpl.java
+++ src/main/java/com/example/mobilele/service/impl/AdminServiceImpl.java
@@ -1,6 +1,7 @@
-package com.example.mobilele.service;
+package com.example.mobilele.service.impl;
 
 import com.example.mobilele.model.view.user.UserAdministrationViewModel;
+import com.example.mobilele.service.AdminService;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
