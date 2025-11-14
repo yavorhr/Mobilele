a1f7cba54e03187a73e9b255d07da49ecb7ea6ed
wolfie <yavorhristozov@gmail.com>
2025-11-14 13:18:07 +0200

    Update UserServiceImpl.java

--- src/main/java/com/example/mobilele/service/impl/UserServiceImpl.java
+++ src/main/java/com/example/mobilele/service/impl/UserServiceImpl.java
@@ -22,7 +22,6 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
