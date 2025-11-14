ac7b0f09805032514e210eb34848bb3909a957d0
wolfie <yavorhristozov@gmail.com>
2025-11-14 14:15:28 +0200

    Create UserUpdateStatusResponse.java

---
+++ src/main/java/com/example/mobilele/model/view/UserUpdateStatusResponse.java
@@ -0,0 +1,62 @@
+package com.example.mobilele.model.view;
+
+import com.example.mobilele.model.entity.enums.UserRoleEnum;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+
+public class UserUpdateStatusResponse {
+  private String email;
+  private boolean isEnabled;
+  private Set<UserRoleEnum> roles;
+  private LocalDateTime disabledTime;
+  private boolean accountLocked;
+
+  public UserUpdateStatusResponse() {
+  }
+
+  public LocalDateTime getDisabledTime() {
+    return disabledTime;
+  }
+
+  public boolean isAccountLocked() {
+    return accountLocked;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public boolean isEnabled() {
+    return isEnabled;
+  }
+
+  public Set<UserRoleEnum> getRoles() {
+    return roles;
+  }
+
+  public UserUpdateStatusResponse setEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
+  public UserUpdateStatusResponse setEnabled(boolean enabled) {
+    isEnabled = enabled;
+    return this;
+  }
+
+  public UserUpdateStatusResponse setRoles(Set<UserRoleEnum> roles) {
+    this.roles = roles;
+    return this;
+  }
+
+  public UserUpdateStatusResponse setDisabledTime(LocalDateTime disabledTime) {
+    this.disabledTime = disabledTime;
+    return this;
+  }
+
+  public UserUpdateStatusResponse setAccountLocked(boolean accountLocked) {
+    this.accountLocked = accountLocked;
+    return this;
+  }
+}
