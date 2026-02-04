e570b59ebada073165de9403f1c05316bf0699bd
wolfie <yavorhristozov@gmail.com>
2026-02-04 15:06:52 +0200

    Update PictureService.java

--- src/main/java/com/example/mobilele/service/PictureService.java
+++ src/main/java/com/example/mobilele/service/PictureService.java
@@ -8,8 +8,6 @@ import java.io.IOException;
 
 public interface PictureService {
 
-//  List<Picture> addOfferPictures(List<MultipartFile> pictures) throws IOException;
-
   Picture findById(Long id);
 
   void addPicturesToOffer(PicturesAddServiceModel serviceModel) throws IOException;
