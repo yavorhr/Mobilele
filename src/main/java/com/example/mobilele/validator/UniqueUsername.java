5530405d70eaa321a67c6545a3ca94259acdc92d
wolfie <yavorhristozov@gmail.com>
2026-02-09 16:02:02 +0200

    Update UniqueUsername.java
    change error message

--- src/main/java/com/example/mobilele/validator/UniqueUsername.java
+++ src/main/java/com/example/mobilele/validator/UniqueUsername.java
@@ -12,7 +12,7 @@ import java.lang.annotation.Target;
 @Constraint(validatedBy = UniqueUsernameValidator.class)
 public @interface UniqueUsername {
 
-  String message() default "Username is already occupied!";
+  String message() default "Username already exists";
 
   Class<?>[] groups() default { };
 
