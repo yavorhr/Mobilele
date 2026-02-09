bbc2f761d38a302aa8322cebcf3c2eb0dcc134a4
wolfie <yavorhristozov@gmail.com>
2026-02-09 16:01:32 +0200

    Update UniquePhoneNumber.java
    fix bug - missing constraint

--- src/main/java/com/example/mobilele/validator/UniquePhoneNumber.java
+++ src/main/java/com/example/mobilele/validator/UniquePhoneNumber.java
@@ -1,10 +1,18 @@
 package com.example.mobilele.validator;
 
+import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Target({ElementType.METHOD})
+@Retention(RetentionPolicy.RUNTIME)
+@Constraint(validatedBy = PhoneNumberValidator.class)
 public @interface UniquePhoneNumber {
 
-  String message() default "Phone number already exists!";
+  String message() default "Phone number already exists";
 
   Class<?>[] groups() default {};
 
