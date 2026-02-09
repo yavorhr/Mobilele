e747845959acf3298a8d005e05ad1b0b5616503c
wolfie <yavorhristozov@gmail.com>
2026-02-09 13:50:56 +0200

    Create Constants.java

---
+++ src/main/java/com/example/mobilele/util/Constants.java
@@ -0,0 +1,14 @@
+package com.example.mobilele.util;
+
+public class Constants {
+
+  private Constants() {
+    // prevent instantiation
+  }
+
+  // Contexts for dynamic header
+  public static final String CONTEXT_BRAND = "brand";
+  public static final String CONTEXT_MODEL = "model";
+  public static final String CONTEXT_FAVORITES = "favorites";
+  public static final String CONTEXT_MY = "my";
+}
