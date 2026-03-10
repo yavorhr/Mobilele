4a3c7013c84ecaf167e8bda07a795d5140659ba3
wolfie <yavorhristozov@gmail.com>
2026-03-10 11:31:23 +0200

    Update ApplicationSecurityConfiguration.java
    broadening the rule

--- src/main/java/com/example/mobilele/config/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/mobilele/config/ApplicationSecurityConfiguration.java
@@ -37,7 +37,7 @@ public class ApplicationSecurityConfiguration {
                     requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll().
                     requestMatchers(EndpointRequest.to("health", "info")).permitAll().
                     requestMatchers("/", "/error", "/users/login", "/users/register", "/logger/**", "/models/**").permitAll().
-                    requestMatchers("/statistics").hasRole(UserRoleEnum.ADMIN.name()).
+                    requestMatchers("/admin/**").hasRole(UserRoleEnum.ADMIN.name()).
                     requestMatchers("/static/**").permitAll().
                     requestMatchers("/**").authenticated())
             .formLogin(form -> form
