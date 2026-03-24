b5e30e7950825b427bb0caa17b8995e4ad89fd2c
wolfie <yavorhristozov@gmail.com>
2026-03-24 12:35:38 +0200

    Update MobileleUserServiceImplTest.java
    fix bug

--- src/test/java/com/example/mobilele/service/impl/principal/MobileleUserServiceImplTest.java
+++ src/test/java/com/example/mobilele/service/impl/principal/MobileleUserServiceImplTest.java
@@ -4,14 +4,11 @@ import com.example.mobilele.model.entity.UserEntity;
 import com.example.mobilele.model.entity.UserRoleEntity;
 import com.example.mobilele.model.entity.enums.UserRoleEnum;
 import com.example.mobilele.repository.UserRepository;
-
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
@@ -20,7 +17,8 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class MobileleUserServiceImplTest {
@@ -33,7 +31,6 @@ class MobileleUserServiceImplTest {
 
   @InjectMocks
   private MobileleUserServiceImpl userService;
-
 
   @BeforeEach
   void init() {
@@ -51,21 +48,22 @@ class MobileleUserServiceImplTest {
     userRole.setRole(UserRoleEnum.USER);
 
     user.setRoles(List.of(adminRole, userRole));
-
-    Mockito.when(userRepository
-            .findByUsername(user.getUsername()))
-            .thenReturn(Optional.of(user));
   }
 
   @Test
   public void testLoadUserByUsername_UserExists() {
+    // Arrange
+    when(userRepository
+            .findByUsername(user.getUsername()))
+            .thenReturn(Optional.of(user));
+
     // Act
      userDetails = userService.loadUserByUsername("user");
 
     // Assert
-    Assertions.assertNotNull(userDetails);
-    Assertions.assertEquals("user", userDetails.getUsername());
-    Assertions.assertEquals("password", userDetails.getPassword());
+    assertNotNull(userDetails);
+    assertEquals("user", userDetails.getUsername());
+    assertEquals("password", userDetails.getPassword());
 
     String expectedRoles = "ROLE_ADMIN, ROLE_USER";
     String actualRoles = userDetails.getAuthorities()
@@ -73,16 +71,16 @@ class MobileleUserServiceImplTest {
             .map(GrantedAuthority::getAuthority).collect(
                     Collectors.joining(", "));
 
-    Assertions.assertEquals(userDetails.getUsername(), user.getUsername());
-    Assertions.assertEquals(expectedRoles, actualRoles);
+    assertEquals(userDetails.getUsername(), user.getUsername());
+    assertEquals(expectedRoles, actualRoles);
   }
 
  @Test
  public void testUserNotFound() {
-   Mockito.when(userRepository.findByUsername("missingUser"))
+   when(userRepository.findByUsername("missingUser"))
            .thenReturn(Optional.empty());
 
-    Assertions.assertThrows(
+    assertThrows(
             UsernameNotFoundException.class,
             () -> userService.loadUserByUsername("missingUser")
     );
@@ -96,20 +94,24 @@ class MobileleUserServiceImplTest {
             new SimpleGrantedAuthority("ROLE_ADMIN")
     );
 
+    when(userRepository
+            .findByUsername(user.getUsername()))
+            .thenReturn(Optional.of(user));
+
     // Act
     userDetails = userService.loadUserByUsername("user");
     MobileleUser mobileleUser = new MobileleUser(user, authorities);
 
     // Assert
-    Assertions.assertEquals(1L, ((MobileleUser) userDetails).getId());
-    Assertions.assertEquals(user.getUsername(), userDetails.getUsername());
-    Assertions.assertEquals(user.getPassword(), userDetails.getPassword());
-    Assertions.assertTrue(userDetails.isEnabled());
-    Assertions.assertTrue(userDetails.isAccountNonExpired());
-    Assertions.assertTrue(userDetails.isCredentialsNonExpired());
-    Assertions.assertTrue(userDetails.isAccountNonLocked());
-    Assertions.assertEquals(2, userDetails.getAuthorities().size());
-    Assertions.assertEquals(mobileleUser.getUserEntity(), user);
-  }
+   assertEquals(1L, ((MobileleUser) userDetails).getId());
+   assertEquals(user.getUsername(), userDetails.getUsername());
+   assertEquals(user.getPassword(), userDetails.getPassword());
+   assertTrue(userDetails.isEnabled());
+   assertTrue(userDetails.isAccountNonExpired());
+   assertTrue(userDetails.isCredentialsNonExpired());
+   assertTrue(userDetails.isAccountNonLocked());
+   assertEquals(2, userDetails.getAuthorities().size());
+   assertEquals(mobileleUser.getUserEntity(), user);
 
+  }
 }
