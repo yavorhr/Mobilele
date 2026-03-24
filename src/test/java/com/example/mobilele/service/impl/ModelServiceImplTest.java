78de76a3385655edcecfee151db1048d3d8f47eb
wolfie <yavorhristozov@gmail.com>
2026-03-24 14:36:19 +0200

    Update ModelServiceImplTest.java
    testFindByName_NotFound()

--- src/test/java/com/example/mobilele/service/impl/ModelServiceImplTest.java
+++ src/test/java/com/example/mobilele/service/impl/ModelServiceImplTest.java
@@ -3,16 +3,16 @@ package com.example.mobilele.service.impl;
 import com.example.mobilele.model.entity.ModelEntity;
 import com.example.mobilele.repository.ModelRepository;
 import com.example.mobilele.service.BrandService;
+import com.example.mobilele.web.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.Optional;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
@@ -56,5 +56,13 @@ public class ModelServiceImplTest {
 
     assertEquals(model, result);
     assertEquals("X5", result.getName());
+  }
+
+  @Test
+  void testFindByName_NotFound() {
+    when(modelRepository.findByName("N/A")).thenReturn(Optional.empty());
+
+    assertThrows(ObjectNotFoundException.class,
+            () -> modelService.findByName("N/A"));
   }
 }
