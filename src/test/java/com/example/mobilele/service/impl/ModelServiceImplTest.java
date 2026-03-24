b1c7f3f7203f8cb616451140701ad9b3231754d2
wolfie <yavorhristozov@gmail.com>
2026-03-24 15:06:56 +0200

    Update ModelServiceImplTest.java

--- src/test/java/com/example/mobilele/service/impl/ModelServiceImplTest.java
+++ src/test/java/com/example/mobilele/service/impl/ModelServiceImplTest.java
@@ -12,10 +12,10 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
