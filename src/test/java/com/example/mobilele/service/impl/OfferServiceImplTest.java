145279a1576752076421cf11cf39787c46166b1f
wolfie <yavorhristozov@gmail.com>
2026-03-24 15:20:02 +0200

    Update OfferServiceImplTest.java
    testDeleteById()

--- src/test/java/com/example/mobilele/service/impl/OfferServiceImplTest.java
+++ src/test/java/com/example/mobilele/service/impl/OfferServiceImplTest.java
@@ -2,6 +2,7 @@ package com.example.mobilele.service.impl;
 
 import com.example.mobilele.init.OfferSeedGenerator;
 import com.example.mobilele.model.entity.OfferEntity;
+import com.example.mobilele.model.entity.Picture;
 import com.example.mobilele.model.view.offer.OfferViewModel;
 import com.example.mobilele.repository.OfferRepository;
 import com.example.mobilele.repository.SoldOfferRepository;
@@ -10,6 +11,7 @@ import com.example.mobilele.service.BrandService;
 import com.example.mobilele.service.ModelService;
 import com.example.mobilele.service.UserService;
 import com.example.mobilele.util.cloudinary.CloudinaryService;
+import com.example.mobilele.web.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -18,9 +20,11 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.modelmapper.ModelMapper;
 
+import java.util.List;
 import java.util.Optional;
-
+import static org.mockito.Mockito.verify;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.mockito.Mockito.when;
 
@@ -72,5 +76,32 @@ public class OfferServiceImplTest {
     assertTrue(result.isCanModify());
     assertFalse(result.isNotOwnerOrIsAdmin());
     assertTrue(result.isReserved());
+  }
+
+  @Test
+  void testFindOfferById_NotFound() {
+    when(offerRepository.findById(1L)).thenReturn(Optional.empty());
+
+    assertThrows(ObjectNotFoundException.class,
+            () -> offerService.findOfferById("user", 1L));
+  }
+
+  @Test
+  void testDeleteById() {
+    OfferEntity offer = new OfferEntity();
+    offer.setId(1L);
+
+    Picture pic = new Picture();
+    pic.setPublicId("pic1");
+
+    offer.setPictures(List.of(pic));
+
+    when(offerRepository.findById(1L)).thenReturn(Optional.of(offer));
+
+    offerService.deleteById(1L);
+
+    verify(userRepository).deleteFavoritesByOfferId(1L);
+    verify(cloudinaryService).delete("pic1");
+    verify(offerRepository).delete(offer);
   }
 }
