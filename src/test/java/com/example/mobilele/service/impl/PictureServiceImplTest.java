8916d37e737bb7511b6b4ed4b92925a2b5eb9064
wolfie <yavorhristozov@gmail.com>
2026-03-25 15:21:57 +0200

    Update PictureServiceImplTest.java
     void testFindById_NotFound()

--- src/test/java/com/example/mobilele/service/impl/PictureServiceImplTest.java
+++ src/test/java/com/example/mobilele/service/impl/PictureServiceImplTest.java
@@ -5,15 +5,16 @@ import com.example.mobilele.repository.PictureRepository;
 import com.example.mobilele.service.OfferService;
 import com.example.mobilele.service.UserService;
 import com.example.mobilele.util.cloudinary.CloudinaryService;
+import com.example.mobilele.web.exception.ObjectNotFoundException;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -44,5 +45,14 @@ public class PictureServiceImplTest {
     Picture result = pictureService.findById(1L);
 
     assertEquals(1L, result.getId());
+  }
+
+  @Test
+  void testFindById_NotFound() {
+    when(pictureRepository.findById(1L)).thenReturn(Optional.empty());
+
+    assertThrows(ObjectNotFoundException.class, () -> {
+      pictureService.findById(1L);
+    });
   }
 }
