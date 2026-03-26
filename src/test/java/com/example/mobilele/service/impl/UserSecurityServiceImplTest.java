d04fc28381116c3a331b874deec780466487192c
wolfie <yavorhristozov@gmail.com>
2026-03-26 14:27:06 +0200

    Update UserSecurityServiceImplTest.java

--- src/test/java/com/example/mobilele/service/impl/UserSecurityServiceImplTest.java
+++ src/test/java/com/example/mobilele/service/impl/UserSecurityServiceImplTest.java
@@ -7,9 +7,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-
 import java.util.List;
-
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
