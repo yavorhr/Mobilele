59dfabede2cc086e7eded1acb26561be2148492f
wolfie <yavorhristozov@gmail.com>
2026-03-28 13:53:03 +0200

    Update DataInitializer.java
    add @Profile("!test") to restrict the DB init in test mode

--- src/main/java/com/example/mobilele/init/DataInitializer.java
+++ src/main/java/com/example/mobilele/init/DataInitializer.java
@@ -2,9 +2,11 @@ package com.example.mobilele.init;
 
 import com.example.mobilele.service.*;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
+@Profile("!test")
 @Component
 public class DataInitializer {
   private final UserRoleService userRoleService;
