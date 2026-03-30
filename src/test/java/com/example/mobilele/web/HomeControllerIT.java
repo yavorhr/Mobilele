640076d3d3eb1d4ad63a033e22f8b499f49cdc9d
wolfie <yavorhristozov@gmail.com>
2026-03-30 16:16:28 +0300

    Update HomeControllerIT.java

--- src/test/java/com/example/mobilele/web/HomeControllerIT.java
+++ src/test/java/com/example/mobilele/web/HomeControllerIT.java
@@ -15,9 +15,7 @@ import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-
 import java.util.List;
-
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
