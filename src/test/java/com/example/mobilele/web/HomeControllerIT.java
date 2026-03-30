f538351cef7135bca68b10a1cae726bc855385e7
wolfie <yavorhristozov@gmail.com>
2026-03-30 16:02:27 +0300

    Create HomeControllerIT.java
    void index_shouldReturnIndexViewWithModelAttributes()

---
+++ src/test/java/com/example/mobilele/web/HomeControllerIT.java
@@ -0,0 +1,65 @@
+package com.example.mobilele.web;
+
+import com.example.mobilele.model.view.feedback.FeedbackSummaryViewModel;
+import com.example.mobilele.service.FeedbackService;
+import com.example.mobilele.service.OfferService;
+import com.example.mobilele.service.SoldOfferService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class HomeControllerIT {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private OfferService offerService;
+
+  @MockBean
+  private FeedbackService feedbackService;
+
+  @MockBean
+  private SoldOfferService soldOfferService;
+
+  @Test
+  void index_shouldReturnIndexViewWithModelAttributes() throws Exception {
+
+    when(offerService.findLatestOffers(5)).thenReturn(List.of());
+    when(offerService.findMostViewedOffers(5)).thenReturn(List.of());
+    when(feedbackService.findRecentFeedbacks(10)).thenReturn(List.of());
+    when(feedbackService.getFeedbackSummary()).thenReturn(new FeedbackSummaryViewModel());
+    when(soldOfferService.getSoldVehiclesCount()).thenReturn(10L);
+
+    mockMvc.perform(get("/"))
+            .andExpect(status().isOk())
+            .andExpect(view().name("index"))
+            .andExpect(model().attributeExists(
+                    "latestOffers",
+                    "mostViewedOffers",
+                    "feedbacks",
+                    "summary",
+                    "startYear",
+                    "soldCount"
+            ))
+            .andExpect(model().attribute("startYear", 2025))
+            .andExpect(model().attribute("soldCount", 10L));
+  }
+}
