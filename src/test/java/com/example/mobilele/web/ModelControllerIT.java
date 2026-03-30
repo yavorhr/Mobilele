affa354fe7f1ed76e326092f214efd5788d327b4
wolfie <yavorhristozov@gmail.com>
2026-03-30 16:16:45 +0300

    Create ModelControllerIT.java
     void getModels_shouldReturnModelsList()

---
+++ src/test/java/com/example/mobilele/web/ModelControllerIT.java
@@ -0,0 +1,52 @@
+package com.example.mobilele.web;
+
+import com.example.mobilele.model.entity.enums.VehicleCategoryEnum;
+import com.example.mobilele.service.ModelService;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+
+import java.util.List;
+
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ActiveProfiles("test")
+@SpringBootTest
+@AutoConfigureMockMvc
+@Transactional
+public class ModelControllerIT {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private ModelService modelService;
+
+  // =========================
+  // 🔹 SUCCESS CASE
+  // =========================
+
+  @Test
+  void getModels_shouldReturnModelsList() throws Exception {
+
+    when(modelService.findModelsByVehicleTypeAndBrand(
+            "BMW",
+            VehicleCategoryEnum.Car
+    )).thenReturn(List.of("X5", "X6"));
+
+    mockMvc.perform(get("/models")
+            .param("brand", "bmw")
+            .param("vehicleType", "CAR"))
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$[0]").value("X5"))
+            .andExpect(jsonPath("$[1]").value("X6"));
+  }
+}
