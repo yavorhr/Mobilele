8018a7fdfc5366b31d7be8330c30b94d3f412fb7
wolfie <yavorhristozov@gmail.com>
2026-04-02 14:48:06 +0300

    Update OffersApiController.java

--- src/main/java/com/example/mobilele/web/OffersApiController.java
+++ src/main/java/com/example/mobilele/web/OffersApiController.java
@@ -10,7 +10,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.Arrays;
 import java.util.List;
 import java.util.Locale;
