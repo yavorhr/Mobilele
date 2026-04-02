f86111317e86a6f43db862b70d23778903d6f516
wolfie <yavorhristozov@gmail.com>
2026-04-02 14:48:02 +0300

    Update ModelControllerIT.java

--- src/test/java/com/example/mobilele/web/ModelControllerIT.java
+++ src/test/java/com/example/mobilele/web/ModelControllerIT.java
@@ -10,9 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.util.List;
-
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
