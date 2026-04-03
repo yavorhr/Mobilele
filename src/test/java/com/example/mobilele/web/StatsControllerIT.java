81c9b339c530a49c1ee9fc0bf8f053aa0ef9e03d
wolfie <yavorhristozov@gmail.com>
2026-04-03 14:24:49 +0300

    Update StatsControllerIT.java

--- src/test/java/com/example/mobilele/web/StatsControllerIT.java
+++ src/test/java/com/example/mobilele/web/StatsControllerIT.java
@@ -17,14 +17,11 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
-
 import java.util.List;
-
 import static org.mockito.Mockito.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ActiveProfiles("test")
