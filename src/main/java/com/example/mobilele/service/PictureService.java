6473adbdca80f38db6e70aced39aa16ff9e99d0a
wolfie <yavorhristozov@gmail.com>
2026-05-19 14:35:07 +0300

    refact : remove unused dependencies

--- src/main/java/com/example/mobilele/service/PictureService.java
+++ src/main/java/com/example/mobilele/service/PictureService.java
@@ -1,9 +1,7 @@
 package com.example.mobilele.service;
 
 import com.example.mobilele.model.entity.Picture;
-import com.example.mobilele.model.service.offer.PictureAddServiceModel;
 import com.example.mobilele.model.service.user.PicturesAddServiceModel;
-
 import java.io.IOException;
 
 public interface PictureService {
--- src/main/java/com/example/mobilele/web/exception/GlobalExceptionsHandler.java
+++ src/main/java/com/example/mobilele/web/exception/GlobalExceptionsHandler.java
@@ -2,16 +2,11 @@ package com.example.mobilele.web.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 import org.springframework.web.servlet.ModelAndView;
-
 import java.nio.file.AccessDeniedException;
-import java.util.HashMap;
-import java.util.Map;
 
 @ControllerAdvice
 public class GlobalExceptionsHandler {
